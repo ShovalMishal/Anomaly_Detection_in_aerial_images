275bd5e3f9695ad884a276140444e079cab79d37
shoval <shoval@brdcm04-212-pcl.eng.tau.ac.il>
2023-08-01 17:38:51 +0300

    Adding subval where fg and bg are saved seperately and changing hyperparameters to collect all fg

--- create_image_pyramid_patches_data.py
+++ create_image_pyramid_patches_data.py
@@ -1,10 +1,9 @@
 import json
 from argparse import ArgumentParser
-
 import torch
 from torchvision.utils import save_image
 from Anomaly_detection_BG_FG import create_dataloader, create_gaussian_pyramid, split_images_into_patches, \
-    compute_patches_indices_per_scale, show_img
+    compute_patches_indices_per_scale, show_img, get_gt_bboxes_and_labels
 from mmengine.config import Config
 import math
 import os
@@ -16,7 +15,8 @@ from matplotlib import pyplot as plt
 from matplotlib.patches import Polygon, Circle
 from matplotlib.collections import PatchCollection
 import numpy as np
-import Anomaly_Detection_in_aerial_images.DOTA_devkit.dota_utils as util
+from statistics import get_unfounded_fg_areas_n_aspect_ratio, plot_fg_statistics
+import DOTA_devkit.dota_utils as util
 
 def calc_patches_indices(pyramid_levels: int, patch_size: int, scale_factor: float, patch_stride: float, image_size):
     patches_indices_list = []
@@ -31,12 +31,43 @@ def calc_patches_indices(pyramid_levels: int, patch_size: int, scale_factor: flo
     return patches_indices_list
 
 
+def extract_and_save_fgs(gt_instances, image, curr_image_id, scale_factor, dataset_type, dataset_dir, all_labels):
+    patch_num = 0
+    foreground_patches, foreground_labels, fg_patches_indices = get_gt_bboxes_and_labels(gt_instances, image)
+    fg_patches_metadata = {}
+    for patch_ind, patch in enumerate(foreground_patches):
+        level_ind = 0
+        curr_patch_metadata, patch_name = save_patch(patch, patch_num, curr_image_id, scale_factor,
+                                                     fg_patches_indices[0][patch_ind].tolist() + \
+                                                     fg_patches_indices[1][patch_ind].tolist(),
+                                                     level_ind, dataset_type,
+                                                     foreground_labels[patch_ind].item(),
+                                                     dataset_dir, all_labels)
+        fg_patches_metadata[patch_name] = curr_patch_metadata
+        patch_num += 1
+    return patch_num, fg_patches_metadata
+
+
+def save_patch(patch, patch_num, curr_image_id, scale_factor, indices_list,
+               level_ind, dataset_type, label_num, dataset_dir, all_labels):
+    curr_patch_metadata = {}
+    patch_name = curr_image_id + "_" + str(patch_num)
+    curr_patch_metadata["origin_image"] = curr_image_id
+    curr_patch_metadata["scale"] = scale_factor ** level_ind
+    curr_patch_metadata["indices"] = indices_list
+    label = all_labels[label_num] if label_num != -1 else "background"
+    patch_save_path = os.path.join(dataset_dir, dataset_type, "images", label, patch_name + ".png")
+    save_image(torch.flip(patch.to(torch.float32), dims=[0]).unsqueeze(0), patch_save_path, normalize=True)
+    return curr_patch_metadata, patch_name
+
+
 def create_image_pyramid_patches_dataset(args):
     cfg = Config.fromfile(args.config)
     bbox_assigner = TASK_UTILS.build(cfg["patches_assigner"])
     image_size = cfg["train_pipeline"][3]['scale']
-    original_dataset_path = cfg[args.dataset_type+"_dataloader"]["dataset"]["data_root"]
-    original_dataset_anns_path = os.path.join(original_dataset_path, cfg[args.dataset_type+"_dataloader"]["dataset"]["ann_file"])
+    original_dataset_path = cfg[args.dataset_type + "_dataloader"]["dataset"]["data_root"]
+    original_dataset_anns_path = os.path.join(original_dataset_path,
+                                              cfg[args.dataset_type + "_dataloader"]["dataset"]["ann_file"])
     os.makedirs(os.path.join(args.dataset_dir, args.dataset_type, "images"), exist_ok=True)
     os.makedirs(os.path.join(args.dataset_dir, args.dataset_type, "metadata"), exist_ok=True)
     data_loader = create_dataloader(cfg, mode=args.dataset_type)
@@ -47,6 +78,8 @@ def create_image_pyramid_patches_dataset(args):
         os.makedirs(os.path.join(args.dataset_dir, args.dataset_type, "images", label), exist_ok=True)
     patches_indices_list = calc_patches_indices(args.pyramid_levels, args.patch_size, args.scale_factor,
                                                 args.patch_stride, image_size)
+    all_unfounded_fg_areas, all_unfounded_fg_aspect_ratio, all_founded_fg_areas, all_founded_fg_aspect_ratio = \
+        [], [], [], []
     for idx, data_batch in enumerate(data_loader):
         # show image
         # objects = util.parse_dota_poly(os.path.join(original_dataset_anns_path, data_batch['data_samples'][0].img_id+".txt"))
@@ -59,6 +92,12 @@ def create_image_pyramid_patches_dataset(args):
         patch_num = 0
         gt_instances = unpack_gt_instances(data_batch['data_samples'])
         batch_gt_instances, batch_gt_instances_ignore, _ = gt_instances
+        if args.dataset_type == "subval":
+            # save all fg
+            patch_num, fg_patches_metadata = extract_and_save_fgs(batch_gt_instances[0], data_batch['inputs'][0],
+                                                                  curr_image_id, args.scale_factor, args.dataset_type,
+                                                                  args.dataset_dir, all_labels)
+            patches_metadata.update(fg_patches_metadata)
         total_gt_ind = []
         for level_ind, pyramid_level_data in enumerate(pyramid_batch):
             patches = split_images_into_patches(pyramid_level_data,
@@ -70,28 +109,44 @@ def create_image_pyramid_patches_dataset(args):
                 batch_gt_instances_ignore[0])
             assigned_labels = assign_result.labels
             total_gt_ind.append(assign_result.gt_inds)
-
+            if args.dataset_type == "subval":
+                # filter patches to obtain only bgs
+                indices_of_bbs_with_iou_bellow_threshold = torch.squeeze(torch.nonzero(assign_result.gt_inds == 0))
+                assigned_labels = assign_result.labels[indices_of_bbs_with_iou_bellow_threshold]
+                patches = patches[:, indices_of_bbs_with_iou_bellow_threshold]
             for patch_ind in range(patches.shape[1]):
                 patch = patches[0, patch_ind]
-                # if(assigned_labels[patch_ind] != -1):
-                #     show_img(patch, all_labels[assigned_labels[patch_ind]])
-                curr_patch_metadata = {}
-                patch_name = curr_image_id + "_" + str(patch_num)
-                curr_patch_metadata["origin_image"] = curr_image_id
-                curr_patch_metadata["scale"] = args.scale_factor ** level_ind
-                curr_patch_metadata["indices"] = patches_indices_list[level_ind][patch_ind].tolist()
-                label_num = assigned_labels[patch_ind].item()
-                label = all_labels[label_num] if assigned_labels[patch_ind].item()!=-1 else "background"
+                if(assigned_labels[patch_ind] != -1):
+                    show_img(patch, all_labels[assigned_labels[patch_ind]])
+                curr_patch_metadata, patch_name = save_patch(patch, patch_num, curr_image_id, args.scale_factor,
+                                                             patches_indices_list[level_ind][patch_ind].tolist(),
+                                                             level_ind, args.dataset_type,
+                                                             assigned_labels[patch_ind].item(),
+                                                             args.dataset_dir, all_labels)
                 patches_metadata[patch_name] = curr_patch_metadata
-                patch_save_path = os.path.join(args.dataset_dir, args.dataset_type, "images", label, patch_name + ".png")
-                save_image(torch.flip(patch, dims=[0]).unsqueeze(0), patch_save_path, normalize=True)
                 patch_num += 1
         with open(os.path.join(args.dataset_dir, args.dataset_type, "metadata", curr_image_id + ".json"), 'w') as file:
-            json.dump(patches_metadata, file)
+            json.dump(patches_metadata, file, indent=4)
         total_gt_ind = torch.cat(total_gt_ind)
-        print("The number of patches match to unique fg bboxes is " +
-              str(torch.unique(total_gt_ind[torch.nonzero(total_gt_ind > 0)]).shape[0])
-              + " out of " + str(batch_gt_instances[0].labels.shape[0]))
+        if args.dataset_type == "subtrain":
+            fg_gt_bboxes = batch_gt_instances[0].bboxes
+            founded_fg_indices = torch.unique(total_gt_ind[torch.nonzero(total_gt_ind > 0)]) - 1
+            unfounded_areas, unfounded_aspect_ratio, founded_areas, founded_aspect_ratio =\
+                get_unfounded_fg_areas_n_aspect_ratio(fg_gt_bboxes, founded_fg_indices)
+            all_unfounded_fg_areas.append(unfounded_areas)
+            all_unfounded_fg_aspect_ratio.append(unfounded_aspect_ratio)
+            all_founded_fg_areas.append(founded_areas)
+            all_founded_fg_aspect_ratio.append(founded_aspect_ratio)
+            number_of_all_filtered_fg = torch.nonzero(unfounded_areas > 1000).size(0) + founded_areas.size(0)
+            print(f"({idx}) The number of patches match to unique fg bboxes is " +
+                  str(founded_fg_indices.shape[0])
+                  + " out of " + str(number_of_all_filtered_fg))
+    if args.dataset_type == "subtrain":
+        plot_fg_statistics(torch.cat(all_unfounded_fg_areas), torch.cat(all_unfounded_fg_aspect_ratio),
+                           is_unfounded=True)
+        plot_fg_statistics(torch.cat(all_founded_fg_areas), torch.cat(all_founded_fg_aspect_ratio),
+                           is_unfounded=False)
+
 
 def showAnns(objects, img):
     """
@@ -130,11 +185,11 @@ def main():
     parser = ArgumentParser()
     parser.add_argument("-c", "--config", help="The relative path to the cfg file")
     parser.add_argument("-d", "--dataset_dir", help="The saved model path")
-    parser.add_argument("-l", "--pyramid_levels", default=5, help="Number of pyramid levels")
-    parser.add_argument("-sf", "--scale_factor", default=0.6, help="Scale factor between pyramid levels")
+    parser.add_argument("-l", "--pyramid_levels", default=6, help="Number of pyramid levels")
+    parser.add_argument("-sf", "--scale_factor", default=0.7, help="Scale factor between pyramid levels")
     parser.add_argument("-po", "--patch_stride", default=0.1, help="Stride between patches in %")
     parser.add_argument("-ps", "--patch_size", default=50, help="The patch size")
-    parser.add_argument("-dt", "--dataset_type", default="subval", help="The dataset type (train or test)")
+    parser.add_argument("-dt", "--dataset_type", default="subtrain", help="The dataset type (train or test)")
     args = parser.parse_args()
     create_image_pyramid_patches_dataset(args)
 
